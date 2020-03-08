114801dafc77efa1333c960ff277a73a61cbbf1d
Skantz <danska@kth.se>
2020-03-08 13:14:17 +0900

    add lemon as submodule

--- lemon-1.3.1/build/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/Dropbox/kth/kth-dd2467-individual-project/clustering/lemon-1.3.1/doc
-# Build directory: /home/daniel/Dropbox/kth/kth-dd2467-individual-project/clustering/lemon-1.3.1/build/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
